e4e40b0e5ab8ad3c29fc71826cc2241f48658345
jinghaihan <jhh19980114@gmail.com>
2026-03-01 18:45:41 +0800

    feat: route transition

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,8 @@
 import { defineConfig } from '@octohash/eslint-config'
 
 export default defineConfig({
+  astro: true,
   unocss: true,
   formatters: true,
-  ignores: [
-    './src/markdown/posts/concepts/javascript-core-concepts.mdx',
-    './src/markdown/posts/concepts/javascript-handwriting-concepts.mdx',
-    './src/markdown/posts/concepts/typescript-core-concepts.mdx',
-  ],
+  ignores: [],
 })
