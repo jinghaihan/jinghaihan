3773b0a710b84636e380932bebc9ce2ad0d28ee3
jinghaihan <jhh19980114@gmail.com>
2026-03-02 01:23:58 +0800

    feat: roadmap page

---
+++ src/components/algorithm/roadmap.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import Workflow from '../workflow.vue'
+</script>
+
+<template>
+  <Workflow />
+</template>
---
+++ src/components/workflow.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { Background } from '@vue-flow/background'
+import { VueFlow } from '@vue-flow/core'
+import '@vue-flow/core/dist/style.css'
+import '@vue-flow/core/dist/theme-default.css'
+</script>
+
+<template>
+  <div class="size-full">
+    <VueFlow
+      :nodes="[]"
+      :edges="[]"
+      :pan-on-scroll="true"
+      :zoom-on-scroll="false"
+      :zoom-on-pinch="true"
+      :zoom-on-double-click="true"
+      :min-zoom="0.2"
+      :max-zoom="3"
+      class="size-full"
+    >
+      <Background :size="2" color="var(--input)" />
+    </VueFlow>
+  </div>
+</template>
